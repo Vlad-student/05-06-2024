e3ceacedc16164d034e592792ce8bdfb9c1c0e6d
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-06-09 13:16:11 +0300

    Examples for while

---
+++ index.js
@@ -0,0 +1,72 @@
+// let amountPlate = 0;
+// const totalAmountPlate =5;
+
+// while (amountPlate<totalAmountPlate) {
+//     amountPlate++;
+//     console.log('washing plate ' , amountPlate);
+// }
+
+// while (true) {
+//     const userInput = prompt= ("enter number 77");
+//     if (userInput==='77') {
+//         alert('thanks');
+//         break;
+//     }
+// }
+
+// while (true) {
+//     const userInput= prompt ('enter number multiply 7');
+//     const userNumber = Number(userInput);
+//     console.log(userNumber);
+//     if (userInput%7===0 && userInput!=='' && userInput !== null) {
+//       alert('thanks');
+//     break;  
+//     }
+// }
+
+// while (true) {
+//     const userInput=prompt('enter number 0');
+//     const userNumber = '0' ;
+//     if (userInput===userNumber ) {
+//         alert('thanks');
+//         break;
+//     }
+// }
+
+// while (true) {
+//     const userInput = prompt('enter something');
+//     if (userInput) {
+//         alert('thanks');
+//         break;
+//     }
+// }
+
+// const secretNumber = 4;
+// while (true) {
+//     const userInput = prompt('enter secret number');
+//     const userNumber = Number(userInput);
+//     if (userNumber===secretNumber){
+//         alert('thanks');
+//         break;
+//     }
+//     else if(userInput==='' || userInput!==null ){
+//         alert('error, please enter number ');
+
+//     }else if (userNumber>secretNumber){
+//         alert('secret number less on');
+//     }else {
+//         alert('secret number bigger' );
+//     }
+// }
+
+
+for (let i=0;i<5;i++){
+    console.log(i);
+}
+
+
+let i=0
+while (i<5) {
+    console.log(i);
+    i++;
+}
